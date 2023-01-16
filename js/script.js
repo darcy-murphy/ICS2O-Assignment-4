d6afe83a4bfbc2bd06259ae9ca0ae93a48fb89e9
darcy-murphy <113056025+darcy-murphy@users.noreply.github.com>
2023-01-15 19:54:06 -0500

    update

--- js/script.js
+++ js/script.js
@@ -1,43 +1,40 @@
-
-
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2022 Timothy Manwell All rights reserved
 //
-// Created by: Darcy Murphy
+// Created by: Timothy Manwell
 // Created on: Nov 2022
 // This file contains the JS functions for index.html
 
+"use strict"
+
 /**
- * Check servie worker.
+ * Check service worker.
  */
- if (navigator.serviceWorker) {
-  navigator.serviceWorker.register(
-    "/ICS2O-Unit5-01/sw.js",
-    {
-      scope:"/ICS2O-Unit5-01/",
-    }
-  )
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Assignment-4/sw.js", {
+    scope: "/ICS2O-Assignment-4/",
+  })
 }
 
 /**
- * This function displays the slider value.
+ * This function displays an alert.
  */
 function myButtonClicked() {
-  document.getElementById("slider-value").innerHTML =
-    "<p>Value is: " + slider.value + "</p>"
-}
+  const TAX = 1.13
+  const message = 3.99
+  const delivery = 15
+  const size = parseFloat(document.getElementById("size").value)
+  const flavour = parseFloat(document.getElementById("flavour").value)
+  const messageAnswer = document.getElementById("message").value
+  const pickUpOrDelivery = document.getElementById("pick-up-or-delivery").value
 
-const randomNumber = Math.floor(Math.random() * 6) + 1
+  const basePriceNoTAX = size + flavour
+  const basePrice = basePriceNoTAX * TAX
+  const messageOnly = (basePriceNoTAX + message) * TAX
+  const deliveryOnly = (basePriceNoTAX + delivery) * TAX
+  const messageAndDelivery = (basePriceNoTAX + message + delivery) * TAX
 
-/** 
- * This function updates the slider value.
- */
-function updateSliderValue(valueFromSlider) {
-  document.getElementById("slider-value").innerHTML = valueFromSlider
-  document.getElementById("answer").innerHTML =
-    "The answer was, " + randomNumber + "!" + " You got it! Good job."
-  
-    // block of code to be executed if conditional is true
-  if (valueFromSlider != randomNumber) 
-    document.getElementById("answer").innerHTML = 
-    "The answer was, " + randomNumber + " !" + " Nice guess, but try again."
+  else {
+    document.getElementById("total").innerHTML =
+      "Your total is: $" + basePrice.toFixed(2) + " including tax."
   }
+}
